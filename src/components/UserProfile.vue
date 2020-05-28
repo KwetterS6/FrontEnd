01648b9b70c8bf15a8aa5727a5f8a61d23483851
Wouter Vandenboorn <wouter04316@gmail.com>
2020-05-28 16:49:45 +0200

    working get

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,8 +1,14 @@
 <template>
 <div class="contain">
+
+
+
   <div style="margin: 25px 0px;">
     <b-overlay :show="show" rounded="sm">
       <b-card title="My Profile" :aria-hidden="show ? 'true' : null">
+        <ul>
+          <li v-for="kwet in kwets" :key="kwet.message"> {{kwet.message}}</li>
+        </ul>
         <b-card-text>*ProfileName*</b-card-text>
         <b-card-text>*User-Details*</b-card-text>
          <b-button :disabled="show" variant="primary" @click="show = true">
@@ -30,11 +36,32 @@
       <b-card img-src="https://placekitten.com/150/150" img-alt="Card image" img-left class="mb-3" title="Username" sub-title="03/05/2020 09:12"  align="left">
       <b-card-text>
         My first kwet!
+
+              
       </b-card-text>
       </b-card>
         </div>
       </b-card>
     </b-overlay>
+  </div>
+   <div> 
+    <b-overlay rounded="sm">
+      <b-card title="Recent Kwets">
+
+        <div v-for="kwet in kwets" :key="kwet.message">
+        <div class="recentkwets">
+      <b-card img-src="https://placekitten.com/150/150" img-alt="Card image" img-left class="mb-3" :title= kwet.userId :sub-title= kwet.timeStamp align="left" >
+      <b-card-text >
+        {{kwet.message}}
+      </b-card-text>
+      </b-card>
+        </div>
+        </div>
+
+      </b-card>
+    </b-overlay>
+
+
   </div>
 </div>
 </template>
@@ -42,16 +69,27 @@
 <script>
 
 export default {
-  name: 'app',
+  name: 'UserProfile',
   components: {
   },
   data() {
     return {
-      
+      kwets: []
     }
   },
   methods: {
-    
+    async getKwets()
+      {
+        const requestOptions = {
+          method: "GET",
+        }
+        let response = await fetch('http://localhost:5002/kwet', requestOptions)
+        let body = await response.json()
+        this.kwets = body
+      }
+  },
+  async mounted(){
+    await this.getKwets()
   }
 }
 </script>
