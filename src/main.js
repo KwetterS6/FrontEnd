75f11891538a2777a9571a7c89332ad417a81c9d
Wouter Vandenboorn <wouter04316@gmail.com>
2020-06-17 08:24:10 +0200

    added sentry

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
+
+Sentry.init({
+  dsn: 'https://87d2919f38e04833acbdd85b0b3d4291@o408386.ingest.sentry.io/5279141',
+  integrations: [new VueIntegration({Vue, attachProps: true})],
+});
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
@@ -12,3 +20,4 @@ new Vue({
   router,
    render: h => h(App)
 }).$mount('#app')
+
